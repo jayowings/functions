d38375af29a043536e85d9cb9194a0c3a82e5103
Jay Owings <jaydenowings@mail.weber.edu>
2021-04-05 09:11:29 -0600

    Refactored the menu

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 # Get info from users
-# TODO user input validation
+# TODO user input validation for menu and numvber
 # TODO convert string to number
+# Menu system 1-inches to mm 2-mm to inches
+userConversionInput = input ('What type of conversion? \n\t 1 - inches to mm; \n\t 2 - mm to inches \n')
 userInput = input('What is the number: ')
 print ('Your number is:', userInput) # TODO remove for production
 userNumber = float(userInput) #
@@ -8,10 +10,14 @@ userNumber = float(userInput) #
 # Perform the conversion
 
 # Convert in to mm 25.4 mm per in
-# userAnswer = userNumber * 25.4
+if userConversionInput == '1':
+    print ('in to mm')
+    userAnswer = userNumber * 25.4
 
 # Convert from mm to in
-userAnswer = userNumber / 25.4
+if userConversionInput == '2':
+    print ('mm to in')
+    userAnswer = userNumber / 25.4
 
 # Print out the answer
 print ('The answer is: ', userAnswer)
