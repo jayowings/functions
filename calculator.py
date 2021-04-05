70eb9ace6da445cf22e9cbbd4d999e1f09efc185
Jay Owings <jaydenowings@mail.weber.edu>
2021-04-05 08:54:44 -0600

    Initial Conversion Calculations

--- calculator.py
+++ calculator.py
@@ -3,7 +3,15 @@
 # TODO convert string to number
 userInput = input('What is the number: ')
 print ('Your number is:', userInput) # TODO remove for production
-userNumber = int(userInput) #
+userNumber = float(userInput) #
 
 # Perform the conversion
+
+# Convert in to mm 25.4 mm per in
+# userAnswer = userNumber * 25.4
+
+# Convert from mm to in
+userAnswer = userNumber / 25.4
+
 # Print out the answer
+print ('The answer is: ', userAnswer)
