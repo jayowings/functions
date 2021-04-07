da92f5f7c03f93a55efdc79a6eebea8c96912169
Jay Owings <jaydenowings@mail.weber.edu>
2021-04-07 09:10:56 -0600

    added while loop

--- calculator.py
+++ calculator.py
@@ -1,25 +1,37 @@
 # Get info from users
 # TODO user input validation for menu and numvber
 # TODO convert string to number
-# Menu system 1-inches to mm 2-mm to inches
-userConversionInput = input ('What type of conversion? \n\t 1 - inches to mm; \n\t 2 - mm to inches \n')
-userInput = input('What is the number: ')
-print ('Your number is:', userInput) # TODO remove for production
-userNumber = float(userInput) #
+#conversion facts
+def MMToIn(Inputt):
+    return Inputt / 25.4
+def InToMM(Inputt):
+    return Inputt * 25.4
+def InToFt(Inputt):
+    return Inputt /12
 
+# Perform the conversion
 def printResults(userConversionInput, userInputx):
     if userConversionInput == '1':
-        userAnswer = userNumber * 25.4
+        userAnswer = InToMM(userNumber)
         conversionUnit = ("in")
         ConvertedUnit = ('mm')
     if userConversionInput == '2':
-        userAnswer = userNumber / 25.4
-        conversionUnit = ('mm ')
-        ConvertedUnit = ('in')
+            userAnswer = MMToIn(userNumber)
+            conversionUnit = ('mm ')
+            ConvertedUnit = ('in')
+    if userConversionInput == '3':
+            userAnswer = InToFt(userNumber)
+            conversionUnit = ('in')
+            ConvertedUnit = ('ft')
     print ("The answer is", userInputx, conversionUnit, "=", userAnswer, ConvertedUnit + ".")
 
-# Perform the conversion
-
-# Print out the answer
-
-printResults(userConversionInput, userInput)
+while True:
+    userConversionInput = input ('What type of conversion? \n\t 1 - inches to mm \n\t 2 - mm to inches \n\t 3 - inches to feet \n\t Q to Quit \n')
+    if userConversionInput == "Q":
+        break
+    userInput = input('What is the number: ')
+    print ('Your number is:', userInput) # TODO remove for production
+    userNumber = float(userInput) #
+    
+    # Print out the answer
+    printResults(userConversionInput, userInput)
